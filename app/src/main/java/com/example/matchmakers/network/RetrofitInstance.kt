f8e01b95ad0f57b3323b5b1b615e42303bcac9f3
beibeit <beibeit@sfu.ca>
2024-12-02 04:56:26 -0800

    update the ML model

--- app/src/main/java/com/example/matchmakers/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/matchmakers/network/RetrofitInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
     // Ensure the Flask server is accessible from the Android device  (make it public)
-    private const val BASE_URL = "http://0.0.0.0:5000"
+    private const val BASE_URL = "https://recommand-sys-408256072995.us-central1.run.app"
     val api: ClusterApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
